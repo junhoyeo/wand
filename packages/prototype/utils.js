ec4a8a9c73f6a26fb1361f836244c2cb6908bc48
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-09 01:13:11 +0900

    fix: styles for mobile view

--- packages/prototype/utils.js
+++ packages/prototype/utils.js
@@ -1,5 +1,5 @@
 // change rootURL by environment
 const rootURL = location.href.includes('github.io') ?
-  'https://junhoyeo.github.io/wand-poc' : '..'
+  'https://junhoyeo.github.io/wand-prototype' : '..'
 
 const gup = Object.fromEntries(new URL(location.href).searchParams)
