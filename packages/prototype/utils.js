3e34a389d36e7465ce62ce7b18887dfcf87d8338
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-08 16:17:33 +0900

    fix: detailed rootURL switching

--- packages/prototype/utils.js
+++ packages/prototype/utils.js
@@ -1,5 +1,5 @@
 // change rootURL by environment
-const rootURL = location.href.includes('localhost') ?
-  '..' : 'https://junhoyeo.github.io/wand-poc'
+const rootURL = location.href.includes('github.io') ?
+  'https://junhoyeo.github.io/wand-poc' : '..'
 
 const gup = Object.fromEntries(new URL(location.href).searchParams)
