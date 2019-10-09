922678bb8af259d362e72c96c978a9bdf5f2359b
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-09 14:19:59 +0900

    feat: show skeleton for result cards

--- packages/prototype/screens/result/result.js
+++ packages/prototype/screens/result/result.js
@@ -5,6 +5,34 @@ const searchInput = document.getElementById('search-input')
   searchInput.value = text
 })(query);
 
+function renderSkeletonCard(cardListID) {
+  const skeleton = document.createElement('div')
+  const cardList = document.getElementById(cardListID)
+  skeleton.className = 'skeleton-card'
+  skeleton.innerHTML = `
+    <div class="skeleton-image"></div>
+    <div class="skeleton-info">
+      <div class="skeleton-title"></div>
+      <div class="skeleton-desc">
+        <div class="skeleton-line"></div>
+        <div class="skeleton-line"></div>
+        <div class="skeleton-line"></div>
+      </div>
+    </div>
+    <div
+      class="skeleton-more"
+      target="_blank"
+    ></div>`
+  cardList.insertBefore(skeleton, document.getElementById('card-end'))  
+}
+
+function hideSkeletonCards() {
+  const skeletons = Array.prototype.slice.call(document.getElementsByClassName('skeleton-card'))
+  skeletons.forEach((element) => {
+    element.parentNode.removeChild(element)
+  })
+}
+
 function renderResultCard(result, cardListID) {
   const card = document.createElement('div')
   const cardList = document.getElementById(cardListID)
@@ -25,10 +53,16 @@ function renderResultCard(result, cardListID) {
   cardList.insertBefore(card, document.getElementById('card-end'))
 }
 
+;(function initSkeleton() {
+  renderSkeletonCard('event-cards')
+  renderSkeletonCard('event-cards')
+})();
+
 ;(function initCards() {
   setTimeout(() => {
+    hideSkeletonCards()
     searchResults.forEach(card => {
       renderResultCard(card, 'event-cards')
     })
-  }, 500)
+  }, 800)
 })();
