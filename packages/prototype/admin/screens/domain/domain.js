4083ce5bb8a4bef2e472fbafcaa41ccb61e5c128
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-12 19:30:02 +0900

    feat: edit and save label names

--- packages/prototype/admin/screens/domain/domain.js
+++ packages/prototype/admin/screens/domain/domain.js
@@ -2,6 +2,7 @@ document.body.removeEventListener('click', drawEventListener)
 
 let domains = []
 let currentDomain = {
+  label: '라벨',
   startPoint: [3, 3],
   endPoint: [3, 3],
 }
@@ -22,32 +23,42 @@ function setDomainState(x, y, idx) {
 
 function initCurrentDomain() {
   currentDomain = {
+    label: '라벨',
     startPoint: [3, 3],
     endPoint: [3, 3],
   }
 }
 
 function drawLabel(domain, idx, size=20) {
-  const { startPoint, endPoint } = domain
+  const {
+    startPoint,
+    endPoint,
+    label: labelText
+  } = domain
   const centerX = startPoint[0] + Math.round((endPoint[0] - startPoint[0]) / 2)
   const centerY = startPoint[1] + Math.round((endPoint[1] - startPoint[1]) / 2)
   
   const label = document.createElement('div')
   label.id = `domain-label-${idx}`
   label.className = 'label'
+  label.onmouseover = () => onMouseoverLabel(idx)
   label.innerHTML = `
     <button
       class="delete"
       onclick="onClickDeleteDomain(${idx})"
     >
-      <img src="../../../assets/icons/times-solid.svg">
+      <img class="cancel" src="../../../assets/icons/times-solid.svg">
     </button>
-    <span class="name">
-      <div>
-        <span>라벨</span>
-        <img src="../../../assets/icons/pencil-alt-gray.svg">
-      </div>
-    </span>`
+    <div class="name">
+      <span id="${`domain-name-${idx}`}">
+        ${labelText}
+      </span>
+      <img
+        id="${`domain-icon-${idx}`}"
+        src="../../../assets/icons/pencil-alt-gray.svg"
+        onclick="onClickEditLabel(event, ${idx})"
+      >
+    </div>`
   label.style.left = `calc(${centerX * size}px - 1.5rem)`
   label.style.top = `calc(${centerY * size}px - 0.5rem)`
   const sketchOverlay = document.getElementById('sketch-overlay')
@@ -124,6 +135,65 @@ function onClickDeleteDomain(domainID, splice = true) {
   })
 }
 
+function onClickEditLabel(event, domainID) {
+  const element = event.target
+  const label = document.querySelector(`#domain-label-${domainID} div.name`)
+  const labelName = document.getElementById(`domain-name-${domainID}`)
+
+  if (element.src.includes('pencil')) {
+    // 편집 버튼
+    const labelInputBox = document.createElement('div')
+    labelInputBox.id = `domain-input-${domainID}`
+    labelInputBox.className = 'input'
+    labelInputBox.innerHTML = `
+      <input onkeydown="onKeydownLabelInput(event, ${domainID})"></input>
+    `
+
+    labelName.style.display = 'none';
+    label.insertBefore(labelInputBox, labelName)
+
+    // input
+    const labelInput = labelInputBox.querySelector('input')
+    labelInput.value = labelName.innerText.trim()
+    labelInput.focus()
+
+    element.src = "../../../assets/icons/check-solid.svg"
+  } else {
+    // 편집 내용 저장 버튼
+    const labelInput = document.querySelector(`#domain-input-${domainID} input`)
+    newLabelText = labelInput.value
+    if (!newLabelText) return
+
+    // 다시 라벨 보여주기
+    labelName.innerText = newLabelText
+    labelName.style.display = 'flex'; 
+    element.src = "../../../assets/icons/pencil-alt-gray.svg"
+
+    // 편집 input 박스 삭제
+    labelInput.parentElement.parentNode.removeChild(labelInput.parentElement)
+
+    // domains에 반영, localStorage에 저장
+    domains[domainID].label = newLabelText
+    saveDomains()
+  }
+}
+
+function onKeydownLabelInput(event, domainID) {
+  if (event.keyCode !== 13) return
+  const fakeEvent = {
+    target: document.getElementById(`domain-icon-${domainID}`)
+  }
+  console.log(fakeEvent)
+  onClickEditLabel(fakeEvent, domainID)
+}
+
+function onMouseoverLabel(domainID) {
+  const element = document.getElementById(`domain-label-${domainID}`)
+  const labels = Array.prototype.slice.call(document.querySelectorAll('div.label.hovered'))
+  labels.forEach((label) => label.className = 'label')
+  element.className += ' hovered'
+}
+
 function renderDomains(temp, size=20) {
   temp.forEach((domain, idx) => {
     currentDomain = domain
@@ -148,5 +218,4 @@ function loadDomains(size=20) {
 
 function saveDomains() {
   localStorage.setItem('domains', JSON.stringify(domains))
-  window.alert('저장했습니다.')
 }
