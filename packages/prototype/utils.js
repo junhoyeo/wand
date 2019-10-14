83067ea43e47685c0c89a1c3b96990e305045be5
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-14 20:11:47 +0900

    feat: add level bar in admin page

--- packages/prototype/utils.js
+++ packages/prototype/utils.js
@@ -1,5 +1,5 @@
 // change rootURL by environment
 const rootURL = location.href.includes('github.io') ?
-  'https://junhoyeo.github.io/wand-prototype' : ''
+  'https://junhoyeo.github.io/wand-prototype' : 'http://localhost:8000'
 
 const gup = Object.fromEntries(new URL(location.href).searchParams)
