05e3ddece1d3397b10c660d578b7d946cfba6935
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-16 08:59:25 +0900

    feat: get room cards from api server

--- packages/prototype/main/screens/info/info.js
+++ packages/prototype/main/screens/info/info.js
@@ -11,6 +11,28 @@ const roomElements = {
   desc: document.querySelector('#header-main p.desc'),
 }
 
+function renderCards(cards) {
+  const cardList = document.getElementById('card-list')
+  cards.forEach((card) => {
+    const cardItem = document.createElement('div')
+    cardItem.className = 'card'
+    cardItem.innerHTML = `
+      <img src="${card.image}">
+      <div class="card-info">
+        <span class="name">
+          ${card.name}
+        </span>
+        <span class="type">
+          ${card.type}
+        </span>
+        <span class="price">
+          ${card.price.toLocaleString()}원
+        </span>
+      </div>`
+    cardList.appendChild(cardItem)
+  })
+}
+
 ;(async function initRoom() {
   const { data: { room, point } } = await axios.get(`http://localhost:3000/place/room/0/${roomID}`)
   roomElements.wrap.style.backgroundImage = `url('${room.cover.image}')`
@@ -19,10 +41,13 @@ const roomElements = {
   roomElements.name.innerText = room.name
   roomElements.desc.innerText = room.desc
   roomPoint = point
+  renderCards(room.cards)
 })();
 
 function onClickPath() {
-  if (!roomPoint.length)
+  if (!roomPoint.length) {
     alert('경로를 찾을 수 없습니다.')
+    return
+  }
   location.href = `${rootURL}/main?x=${gup['x']}&y=${gup['y']}&destX=${roomPoint[0]}&destY=${roomPoint[1]}`
 }
