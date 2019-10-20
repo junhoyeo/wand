dad40a5b26c1ff27d2dd5f26985c7b384083ecc1
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-20 11:39:59 +0900

    fix: prototype url

--- packages/prototype/utils.js
+++ packages/prototype/utils.js
@@ -1,5 +1,5 @@
 // change rootURL by environment
-const rootURL = location.href.includes('github.io') ?
-  'https://junhoyeo.github.io/wand-prototype' : 'http://localhost:8000'
+const rootURL = location.href.includes('inudevs.com') ?
+  'http://wandproto.inudevs.com' : 'http://localhost:8000'
 
 const gup = Object.fromEntries(new URL(location.href).searchParams)
